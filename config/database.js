e22dd0a28b95f8f050499af8d4d2cca972c04513
Sophie_Willson <swillson@alumni.risd.edu>
2020-12-10 21:47:50 -0800

    change config

--- config/database.js
+++ config/database.js
@@ -1,31 +1,19 @@
-const config = require('./');
+
+const config = require("./index");
 
 const db = config.db;
-const name = db.name;
+const username = db.username;
 const password = db.password;
 const database = db.database;
 const host = db.host;
 
-console.log(config)
-
 module.exports = {
   development: {
-    name,
+    username,
     password,
     database,
     host,
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
-  },
-  test: {
-    dialect: "sqlite",
-    DB_CONN: "sqlite.memory",
-    logging: false,
-    seederStorage: 'sequelize',
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
+    dialect: "postgres",
+    seederStorage: "sequelize"
   },
 };
